952c48b2867a459a134f43d37adf3fecd997ad7d
Adam Boström <abm19002@student.mdh.se>
2020-03-06 11:31:59 +0100

    Add project files.

---
+++ gittar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace gittar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Alexander!");
+            Console.ReadKey();
+        }
+    }
+}
